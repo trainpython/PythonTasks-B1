d19a2fd6bfb1c87fb37df4ddc2bd7a7b8c739261
Mohan0612 <mohan.chabolu1994@gmail.com>
2022-03-01 12:36:09 +0530

    csv to Customers table

--- JiraTasks/csv_to_db.py
+++ JiraTasks/csv_to_db.py
@@ -1,14 +1,14 @@
 import csv
-import mysql.connector
 import logging
+import Database.db_connect
+import logs.logfile
 
 def csv_to_db(Filename):
-    logging.basicConfig(filename="logs.txt", level=logging.WARNING,
-                        format='%(asctime)s:%(levelname)s:%(message)s:%(lineno)d', datefmt='%d/%m/%Y%I:%M:%S%p')
-    conn = mysql.connector.connect(user='root', password='123456',
-                                   host='localhost',
-                                   database='py_db', port=3306)
-    cursor = conn.cursor()
+    # importing database connection from Database module
+    con=Database.db_connect.db_con()
+    # importing logging from logs module
+    logs.logfile.log()
+    cursor = con.cursor()
     # create_tb="create table Customers (Id int,Name varchar(255),MobileNo bigint,City varchar(255),email varchar(155));"
     # cursor.execute(create_tb)
     try:
@@ -25,7 +25,7 @@ def csv_to_db(Filename):
     except Exception as e:
         logging.error(e)
     finally:
-        conn.commit()
-        conn.close()
+        con.commit()
+        con.close()
 file=input("Enter your file name or file path:")
 csv_to_db(file)
